31075f8056b85ea2bfe25192d91f4422db554793
dongjiahao <841127264@qq.com>
2017-08-15 10:55:11 +0800

    只是想记录一下所写代码行数
    1724

--- 作死/SceneView.swift
+++ 作死/SceneView.swift
@@ -184,8 +184,6 @@ class SceneView: UIViewController {
             
             SCNTransaction.commit()
         }
-        
-        // Do any additional setup after loading the view.
     }
     
     override var shouldAutorotate: Bool {
@@ -206,18 +204,5 @@ class SceneView: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
